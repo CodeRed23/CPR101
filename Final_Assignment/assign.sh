eaa5dca4a66bce2d823f8235e30b3c33642a0edf
Nicholas <39540565+NicholasDefranco@users.noreply.github.com>
2020-04-25 10:03:42 -0400

    Add files via upload

---
+++ Final_Assignment/assign.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+clear
+
+printf "\t\t\t   Diceware\n" 
+
+printf "\t          +=========================+\n"
+printf "\t         /                         /|\n"
+printf "\t        /     *            *      / |\n"
+printf "\t       /                         /  |\n"
+printf "\t      /            *            /   |\n"
+printf "\t     /                         /    |\t    Passphrases Vs.\n"
+printf "\t    /     *            *      /   * |\t      Passwords\n"
+printf "\t   /                         /      |\n"
+printf "\t  +=========================+       |\n"
+printf "\t  |                         |       |\n"
+printf "\t  |                         |	    |\n"
+printf "\t  |     *            *      |       |\n"
+printf "\t  |                         |       +\n"
+printf "\t  |                         |  *   / \n"
+printf "\t  |                         |     /  \n"
+printf "\t  |                         |    /   \n"
+printf "\t  |     *            *      |   /    \n"
+printf "\t  |                         |  /     \n"
+printf "\t  |                         | /      \n"
+printf "\t  +=========================+/       \n"
+printf "\t\t\t          +=========================+\n"
+printf "\t\t\t         /                         /|\n"
+printf "\t\t\t        /     *            *      / |\n"
+printf "\t\t\t       /                         /  |\n"
+printf "\t\t\t      /            *            /   |\n"
+printf "\tPseudo Random        /                         /    |\n"
+printf "\t     Vs.            /     *            *      /   * |\n"
+printf "\tTruly Random       /                         /      |\n"
+printf "\t\t\t  +=========================+       |\n"
+printf "\t\t\t  |                         |       |\n"
+printf "\t\t\t  |                         |	    |\n"
+printf "\t\t\t  |     *            *      |       |\n"
+printf "\t\t\t  |                         |       +\n"
+printf "\t\t\t  |                         |  *   / \n"
+printf "\t\t\t  |                         |     /  \n"
+printf "\t\t\t  |                         |    /   \n"
+printf "\t\t\t  |     *            *      |   /    \n"
+printf "\t\t\t  |                         |  /     \n"
+printf "\t\t\t  |                         | /      \n"
+printf "\t\t\t  +=========================+/       \n"
+
+read conti
+clear
+
+printf "\n\t\t\tWhy Diceware?\n\n"
+
+printf "                     \n"
+printf "                                  \\       \\     /   \\     /    -----+ \n"
+printf "      / / /   / / /                \\       \\   /     \\   /          / \n"
+printf "    -+-+-+- -+-+-+-                 \\       \\ /       \\ /          /  \n"
+printf "    / / /   / / /       -------------+       +         +          /   \n"
+printf "  -+-+-+- -+-+-+-                   /       / \\        |         /    \n"
+printf "  / / /   / / /                    /       /   \\       |        /     \n"
+printf "                                  /       /     \\      |        +----  \n"
+printf "                   \n\n\n"
+
+echo "---------------------------------------------------------------------------"
+
+printf "\t\t\t    Passwords\n\n"
+
+printf "\t +-------    +-----+   \\     /   +-----+    |     --+--  +-------\n"
+printf "\t |          /      |    \\   /    |     |    |       |    |\n"
+printf "\t |          \\      |     \\ /     |     |    |       |    |\n"
+printf "\t |           +-----+      +      +-----+    |       |    |\n"
+printf "\t |               / |      |      |          |       |    |\n"
+printf "\t |              /  |      |      |      ----+----   |    |\n"
+printf "\t +-------      /   |      |      |          |     --+--  +-------\n\n"
+
+printf "n2vc83m8(&*(c890212^!_?defuei\n\n"
+
+printf "Contains lots of symbols and letters excluding whitspace making it\n"
+printf "\t\textremly hard to remember\n\n"
+
+echo "---------------------------------------------------------------------------"
+
+printf "\t\t\t    Passphrase or Passcode\n\n"
+
+printf " +      +  +----  +      +  +----+  +----+  +----+  +---+  |     +---\n"
+printf " | \\   /|  |      | \\   /|  |    |  |    |  |    |  |   |  |     |\n"
+printf " |  \\ / |  |      |  \\ / |  |    |  |    |  |    |  |  /   |     |\n"
+printf " |   +  |  +----  |   +  |  |    |  +----+  +----+  +-+    |     +---\n"
+printf " |      |  |      |      |  |    |  | \\     |    |  |  \\   |     |\n"
+printf " |      |  |      |      |  |    |  |  \\    |    |  |   |  |     |\n"
+printf " |      |  +----  |      |  +----+  |   \\   |    |  +---+  +---- +---\n\n"
+
+printf "Allows whitespace along with dictionary words making them memorable\n\n"
+
+read conti
+clear
+
+printf "\t\t\t    Psuedo Random\n\n"
+
+printf "Don't do this!!!\n\n"
+
+printf "\t\t\t\\#!/bi/n/ba\\sh    /\n"
+
+printf "\t\t\tf\\or /i in {1..10/}\n"
+
+printf "\t\t\tdo\\ /         \\ /\n"
+
+printf "\t\t\tnu/\\m=\$RANDOM  / \n"
+
+printf "\t\t\te/ch\\o -n \"\$nu/m\\ \" >> myPWD\n"
+
+printf "\t\t\t/done\\       /   \\ \n"
+
+printf "\t\t       /      \\     /     \\ \n\n"
+
+printf "DO NOT MAKE A PROGRAM OR A SCRIPT TO GET THE RANDOM NUMBERS!!!\n\n"
+
+printf "Not truly random, requires a seed to start off. \n"
+printf "Performs a mathematical calculation based off the seed\n\n\n"
+
+echo "-------------------------------------------------------------------"
+printf "\t\t\t    Truly Random\n\n"
+
+printf "\t     How do you create a random sequence of numbers?\n"
+
+printf "          +=========================+\n"
+printf "         /                         /|\n"
+printf "        /     *            *      / |\n"
+printf "       /                         /  |\n"
+printf "      /            *            /   |\t\\Here's a clue/\n"
+printf "     /                         /    |\n"
+printf "    /     *            *      /   * |\n"
+printf "   /                         /      |\n"
+printf "  +=========================+       |\n"
+printf "  |                         |       |\n"
+printf "  |                         |	    |\n"
+printf "  |     *            *      |       |\n"
+printf "  |                         |       +\n"
+printf "  |                         |  *   / \n"
+printf "  |                         |     /  \n"
+printf "  |                         |    /   \n"
+printf "  |     *            *      |   /    \n"
+printf "  |                         |  /     \n"
+printf "  |                         | /      \n"
+printf "  +=========================+/       \n"
+
+read conti
+clear
+
+printf "\n\n\n\n"
+
+printf "\t\t    +-----+    +-------+   |        |\n"
+printf "\t\t    |      \\   |       |   |        |\n"
+printf "\t\t    |      /   |       |   |        |\n"
+printf "\t\t    |-----+    |       |   |        |\n"
+printf "\t\t    |\\         |       |   |        |\n"
+printf "\t\t    | \\        |       |   |        |\n"
+printf "\t\t    |  \\       +-------+   +-----   +-----\n\n\n"
+
+printf "\t\t       ---------    |      |    +-----\n"
+printf "\t\t           |        |      |    |\n"
+printf "\t\t           |        |      |    |\n"
+printf "\t\t           |        +------+    +-----\n"
+printf "\t\t           |        |      |    |\n"
+printf "\t\t           |        |      |    |\n"
+printf "\t\t           |        |      |    |\n"
+printf "\t\t           |        |      |    +-----\n\n\n"
+
+printf "\t\t   +--\\     ----+----     +-----    +-----\n"
+printf "\t\t   |   \\        |         |         |\n"
+printf "\t\t   |    \\       |         |         |\n"
+printf "\t\t   |     |      |         |         +-----\n"
+printf "\t\t   |    /       |         |         |\n"
+printf "\t\t   |   /        |         |         |\n"
+printf "\t\t   +--/     ----+----     +-----    +-----\n\n\n\n\n"
+
+printf "\t\t______                             ______\n"
+printf "\t\t\\    /                             \\    /\n"
+printf "\t\t \\  /                               \\  /\n"
+printf "\t\t  \\/                                 \\/\n\n\n"
+
+printf "\t  \\                                                    / \n"
+printf "\t   ----------------------------------------------------   \n"
+
+read conti
+clear
+
